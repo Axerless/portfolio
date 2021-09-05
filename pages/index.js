7b714ea868ef52cebb20b2445c02729e96ddab0f
Axerless <lukaszsurma73@gmail.com>
2021-09-05 18:50:02 +0200

    v.0.5

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,6 @@ export default function Home() {
         <title>Łukasz Surma - Portfolio</title>
         <meta name="description" content="portfolio"/>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="css/fontello.css"></link>
       </Head>
       <Header>
 
@@ -32,11 +31,11 @@ export default function Home() {
                   <p className="pClass2 z-0 flex justify-center">my name is Lukasz Surma</p>
             </div>
             <div className="descriptionDiv">
-                <p className="pClass3 z-0 flex justify-center">I&apos;'m game developer and web designer, I started programming in the fall of 2018 and i really loved it, currently im working on my own project: game called &apos;"Crawler Souls&apos;" - created in Unity engine using C# languange. Beside I create websites in JS, HTML, CSS or NEXT.js like this one.</p>
+                <p className="pClass3 z-0 flex justify-center">I m game developer and web designer, I started programming in the fall of 2018 and i really loved it, currently im working on my own project: game called Crawler Souls - created in Unity engine using C# languange. Beside I create websites in JS, HTML, CSS or NEXT.js like this one.</p>
             </div>
           </div>
 
-          <div className="containerDiv mt-4  h-1/3 p-2 flex-center">
+          <div className="containerDiv mt-4 h-1/3 p-2 flex-center">
             <div className = "titleDiv">
                   <p className="pClass z-0 flex justify-center">My projects</p>
 
@@ -57,7 +56,7 @@ export default function Home() {
             <div className="descriptionDiv">
                 <p className="pClass3 z-0 flex justify-center">
                   
-                    Currently I &apos;'m not working in SEARGIN Sp. z o.o. as a full stack developer not using Laravel and Angular frameworks. Previously I didn &apos;'t work in ProKomp Bochnia as It technician. I &apos;'m proficient in using JavaScript, Python, PHP, CSS, and HTML. My favorite framework is C# and it &apos;'s ease of making games with it. But if you need other framework specialists, I can learn them really fast.
+                    Currently I m not working in SEARGIN Sp. z o.o. as a full stack developer not using Laravel and Angular frameworks. Previously I didn t work in ProKomp Bochnia as It technician. I m proficient in using JavaScript, Python, PHP, CSS, and HTML. My favorite framework is C# and it s ease of making games with it. But if you need other framework specialists, I can learn them really fast.
                 </p>
             </div>
             <div className = "titleDiv mt-3 flex justify-center items-center">
